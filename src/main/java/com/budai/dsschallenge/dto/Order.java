c3d7353df4a55e8ae3a5b292189d49b874a29a47
Attila Budai <budai.attila.istvan@gmail.com>
2020-07-15 22:07:17 +0200

    csv features

--- src/main/java/com/budai/dsschallenge/dto/Order.java
+++ src/main/java/com/budai/dsschallenge/dto/Order.java
@@ -2,16 +2,27 @@ package com.budai.dsschallenge.dto;
 
 
 import com.budai.dsschallenge.data.ProductCode;
+import com.opencsv.bean.CsvBindByName;
 import lombok.Data;
 
 import java.util.Date;
 
+/*
+https://attacomsian.com/blog/spring-boot-upload-parse-csv-file#
+ */
+
 @Data
 public class Order {
+    @CsvBindByName
     private String id;
+    @CsvBindByName
     private ProductCode code;
+    @CsvBindByName
     private int quantity;
+    @CsvBindByName
     private Date deadline;
+    @CsvBindByName
     private int profit;
+    @CsvBindByName
     private int fine;
 }
